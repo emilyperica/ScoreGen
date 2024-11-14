82c652fe096cd6fa4ddcdbca3458b4830451abea
algenioi <ian.algenio@gmail.com>
2024-11-14 18:42:27 -0500

    Issue #148: add pitch, note detection

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,7 +6,7 @@ cmake --build build
 if [[ "$OSTYPE" == "msys"* || "$OSTYPE" == "cygwin"* ]]; then
   ./build/Debug/ScoreGen.exe
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  ./build/Debug/ScoreGen
+  ./build/ScoreGen
 fi
 
 echo -e "\nPress 'Enter' to close this window."
