32e75acc293075986af8f98bf25edd049c8b2c16
algenioi <ian.algenio@gmail.com>
2024-11-16 19:56:36 -0500

    Issue #148: seperate stereo/mono processing, clean up output for PoC

--- src/ScoreGen.cpp
+++ src/ScoreGen.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include "dsp.h"
 
-#define TEST_DATA "test/TestingDatasets/piano-samples/sample-scales/c-major-scale-descending-on-treble-clef.wav"
+#define TEST_DATA "test/TestingDatasets/piano-samples/sample-scales/c-major-scale-on-treble-clef.wav"
 
 int main()
 {
     dsp(TEST_DATA);
 }
-
