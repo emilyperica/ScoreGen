90f49ecc1174e722764a743efada8766d45aa994
Emily Perica <emilyperica@gmail.com>
2024-12-23 18:47:20 -0500

    Issue #190: Adding live audio processing

--- src/ScoreGen.cpp
+++ src/ScoreGen.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include "dsp.h"
+#include "recordAudio.h"
 
 
 #define TEST_DATA "test/TestingDatasets/piano-samples/sample-scales/c-major-scale-on-treble-clef.wav"
 
 int main()
 {
-    dsp(TEST_DATA);
+    // dsp(TEST_DATA);
+    recordAudio();
 }
