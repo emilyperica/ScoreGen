e789247c2a6923d4292c67c362852698d2e51f65
Emily Perica <74936925+emilyperica@users.noreply.github.com>
2025-02-26 13:19:45 -0500

    Issue #184: Filter class and preprocessing for future onset detection (#256)

--- include/detectNoteDuration.h
+++ include/detectNoteDuration.h
@@ -1,12 +1,17 @@
 #ifndef NOTE_DETECTION_H
 #define NOTE_DETECTION_H
 
+#define _USE_MATH_DEFINES
+
 #include <vector>
 #include <string>
 #include <cmath>
 #include <map>
 #include <iostream>
 #include "aubio/aubio.h"
+#include <fftw3.h>
+
+#include "STFT.h"
 
 // Struct to represent a detected note
 struct Note {
@@ -20,5 +25,21 @@ struct Note {
 std::string getNoteName(double freq);
 std::string determineNoteType(float noteDuration, int bpm);
 std::vector<Note> detectNotes(const std::vector<float>& buf, int sample_rate, int channels);
+
+class Filter {
+    public:
+    int ffts;
+    double fs;
+    int bands;
+    double fmin;
+    double fmax;
+    bool equal;
+    std::vector<std::vector<double>> filterbank;
+
+    Filter(int ffts, double fs, int bands = 12, double fmin = 27.5, double fmax = 16000, bool equal = false);
+    std::vector<double> frequencies(int bands, double fmin, double fmax);
+    static std::vector<double> triang(int start, int mid, int stop, bool equal = false);
+};
+
 
 #endif // NOTE_DETECTION_H
