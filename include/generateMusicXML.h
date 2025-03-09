4f159d3f3b146985e9fcba32c13616e84ccc00e2
stoiyeet <98676948+stoiyeet@users.noreply.github.com>
2025-03-09 17:28:57 -0400

    Issue #185: Adding utests for mXML generation, findKey methods (#273)
    * Issue #185: Adding mXML generation tests
    
    * Issue #185: Test constructor params
    
    * Issue #185: Adding findKey utests
    
    * Issue #185: Copy libmusicxml to tests exe fix
    
    ---------
    
    Co-authored-by: algenioi <ian.algenio@gmail.com>

--- include/generateMusicXML.h
+++ include/generateMusicXML.h
@@ -17,46 +17,47 @@ using namespace MusicXML2;
 
 class MusicXMLGenerator 
 {
+    friend class MusicXMLGeneratorTest; // To grant access to this class' private members
 
-public:
-    // Note: some of the below parameters are optional and can be set to null or nullptr while others cannot
-    // see lihbmusicxml.h
-    MusicXMLGenerator(
-        const string& workNumber = "WORN_NUMBER",
-        const string& workTitle = "WORK_TITLE",
-        const string& movementNumber = "MVMT_NUMBER",
-        const string& movementTitle = "MVMNT_TITLE",
-        const string& creatorName = "CREATOR_NAME",
-        const string& creatorType = "CREATOR_TYPE",
-        const string& rightsString = "RIGHTS_STRING",
-        const string& rightsType = "RIGHTS_TYPE",
-        const string& encodingSoftware = "ScoreGen");
+    public:
+        // Note: some of the below parameters are optional and can be set to null or nullptr while others cannot
+        // see lihbmusicxml.h
+        MusicXMLGenerator(
+            const string& workNumber = "WORN_NUMBER",
+            const string& workTitle = "WORK_TITLE",
+            const string& movementNumber = "MVMT_NUMBER",
+            const string& movementTitle = "MVMNT_TITLE",
+            const string& creatorName = "CREATOR_NAME",
+            const string& creatorType = "CREATOR_TYPE",
+            const string& rightsString = "RIGHTS_STRING",
+            const string& rightsType = "RIGHTS_TYPE",
+            const string& encodingSoftware = "ScoreGen");
 
-    ~MusicXMLGenerator();
+        ~MusicXMLGenerator();
 
-    bool generate(
-        const string& outputPath,
-        const vector<XMLNote>& noteSequence,
-        const string& clef,
-        const int& clefLine,
-        const string& timeSignature,
-        const int& keySignature,
-        int divisions);
+        bool generate(
+            const string& outputPath,
+            const vector<XMLNote>& noteSequence,
+            const string& clef,
+            const int& clefLine,
+            const string& timeSignature,
+            const int& keySignature,
+            int divisions);
 
-private:
-    TFactory factory;
-    TElement createScorePart(
-        const string& partId = "P1", 
-        const string& partName = "INSTRUMENT", 
-        const string& partAbbrev = "INST_ABBREV");
+    private:
+        TFactory factory;
+        TElement createScorePart(
+            const string& partId = "P1", 
+            const string& partName = "INSTRUMENT", 
+            const string& partAbbrev = "INST_ABBREV");
 
-    TElement createPart(const vector<XMLNote>& noteSequence,  const string& clef, 
-        const int& clefLine, const string& timeSignature, const int& keySignature, int divisions);
+        TElement createPart(const vector<XMLNote>& noteSequence,  const string& clef, 
+            const int& clefLine, const string& timeSignature, const int& keySignature, int divisions);
 
-    TElement createMeasure(const vector<XMLNote>& measureNotes, int measureNumber, const string& clef, 
-        const int& clefLine, const string& timeSignature, const int& keySignature, int divisions);
+        TElement createMeasure(const vector<XMLNote>& measureNotes, int measureNumber, const string& clef, 
+            const int& clefLine, const string& timeSignature, const int& keySignature, int divisions);
 
-    TElement createNoteElement(const XMLNote& note, int divisions);
+        TElement createNoteElement(const XMLNote& note, int divisions);
 };
 
 #endif
